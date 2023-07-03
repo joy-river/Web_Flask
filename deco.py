2a601f1136c0a7e2cbc3007c9854967fb979c421
harigaze <dlrkd1122@gmail.com>
2023-07-03 15:22:15 +0900

    Advanced Decorator. HTML Parsing.

--- deco.py
+++ deco.py
@@ -2,6 +2,25 @@ import time
 
 current_time = time.time()
 print(current_time)
+def make_bold(func):
+    def wrap_bold():
+        text = f"<b>{func()}</b>"
+        return text
+
+    return wrap_bold
+
+def make_underlined(func):
+    def wrap_under():
+        text = f"<u>{func()}</u>"
+        return text
+
+    return wrap_under
+
+def make_etalic(func):
+    def wrap_etalic():
+        text = f"<em>{func()}</em>"
+        return text
+    return wrap_etalic
 
 
 def speed_calc_decorator(func):
@@ -12,15 +31,15 @@ def speed_calc_decorator(func):
     return wrap_func
 
 
-@speed_calc_decorator
-def fast_function():
-    print(__name__)
-    for i in range(10000000):
-        i * i
-
-
-@speed_calc_decorator
-def slow_function():
-    print(__name__)
-    for i in range(100000000):
-        i * i
+# @speed_calc_decorator
+# def fast_function():
+#     print(__name__)
+#     for i in range(10000000):
+#         i * i
+#
+#
+# @speed_calc_decorator
+# def slow_function():
+#     print(__name__)
+#     for i in range(100000000):
+#         i * i
--- hello_flask.py
+++ hello_flask.py
@@ -1,21 +1,32 @@
 from flask import Flask
-import random
+import deco
 
 app = Flask(__name__)
 
-print(random.__name__)
 
 # 파이썬 데코레이터 -> 어떤 함수에다가 특정한 기능을 일괄적으로 붙이고 싶을때 (전부 2초 지연같이)
 # 말 그대로 데코가 가능한 장식용 함수.
 # @ 기호는 자기 밑에 있는 함수를 데코해주겠다는 뜻.
-@app.route('/') # "/" => 홈페이지로 갈 때. route는 특정 url에서 밑의 함수를 실행하게하는 데코레이터.
+@app.route('/')  # "/" => 홈페이지로 갈 때. route는 특정 url에서 밑의 함수를 실행하게하는 데코레이터.
+@deco.make_bold
+@deco.make_underlined
+@deco.make_etalic
 def Hello_World():
-    return 'Hello, World!'
+    return '<h1 style="text-align:center">Hello, World!</h1>' \
+           '<p>This is P.</p>' \
+           '<img src="https://www.icegif.com/wp-content/uploads/2022/06/icegif-709.gif" width=200>'
+
 
 @app.route('/bye')
 def say_bye():
     return "See you again!"
 
+
+@app.route("/username/<path:name>")
+def greet(name):
+    return f"Greetings, {name}."
+
+
 # __name__ -> 지금 어디서 어떤 모듈을 쓰는지 알려줌. __main__ 은 지금 현재 이 파일 내에서 코드가 실행중이란 뜻.
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
