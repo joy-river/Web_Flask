9f0a66c055a4b03b16d4662060f2c6d31d0eff2a
harigaze <dlrkd1122@gmail.com>
2023-07-12 17:24:17 +0900

    Flask Using Jinja template({{}}, {% %}). Emmet

--- Jinja_Test/server.py
+++ Jinja_Test/server.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template
+from random import randint
+import datetime
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def Init():
-    return render_template("index.html")
+def home():
+    ran_num = randint(1, 100)
+    return render_template("index.html", random_number=ran_num, time_now=datetime.datetime.now().year,
+                           my_name="Joyriver")
+
+
+@app.route("/guess/<path:username>")
+def guess(username):
+    agify = requests.get(url=f"https://api.agify.io?name={username}").json()
+    genderize = requests.get(url=f"https://api.genderize.io/?name={username}").json()
+    return render_template("guess.html", username=username, user_gender=genderize["gender"], user_age=agify["age"])
+
+@app.route("/blog")
+def get_blog():
+    blog_data = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391").json()
+    return render_template("blog.html", blog_data=blog_data)
 
 
 if __name__ == "__main__":
