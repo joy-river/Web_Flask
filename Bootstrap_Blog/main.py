5f09834c39d42e6fce6d8ac550574ab035875e90
harigaze <dlrkd1122@gmail.com>
2023-07-31 17:04:45 +0900

    Udemy Flask(Jinja) Frontend Project.

--- Bootstrap_Blog/main.py
+++ Bootstrap_Blog/main.py
@@ -1,17 +1,26 @@
+import pprint
 from flask import Flask, render_template
-import deco
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return app.redirect("/index.html")
 
 
 @app.route("/<filename>")
 def redirect(filename):
-    return render_template(f"{filename}", filename=filename[0:-5])
+    return render_template(f"{filename}", filename=filename[0:-5], posts=posts)
+
+
+posts = requests.get(url="https://api.npoint.io/fc3f961b4a897e129960").json()
+
+
+@app.route("/post/<int:post_id>")
+def show_a_post(post_id):
+    return render_template("post.html", post=posts[post_id - 1])
 
 
 if __name__ == "__main__":
