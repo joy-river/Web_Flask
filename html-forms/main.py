66877d804242287f8fec62cd381ea3ed254f7592
harigaze <dlrkd1122@gmail.com>
2023-08-01 17:16:53 +0900

    Udemy Flask Project. request data from server.

--- html-forms/main.py
+++ html-forms/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -7,6 +7,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    return f"<h1>Username: {request.form['username']} Password:{request.form['password']}</h1>"
 
 
 app.run(debug=True)
